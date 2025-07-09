1f2588f8c88ff57d3a6902929337d62733bd86be
Yuki Ueno <145101888+Rikishi-com@users.noreply.github.com>
2025-07-09 09:55:50 +0900

    Go for it!!! Soshiro!!!

--- Hyper_Airoco_Bros/Airoco_API/urls.py
+++ Hyper_Airoco_Bros/Airoco_API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Carbon_judge, OneWeekRecordView
+from .views import Carbon_judge, OneWeekRecordView, CarbonTimerView
 
 urlpatterns = [
     path('carbon-judge/', Carbon_judge.as_view(), name='carbon-judge'),
     path('one-week-record/', OneWeekRecordView.as_view(), name='one-week-record'),
+    path('carbon-timer/', CarbonTimerView.as_view(), name='carbon-timer'),
 ]
